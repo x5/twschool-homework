8a87602ff0b46ef2a319da23ec39fe74965364a8
x5 <x5@liushen.me>
2018-04-03 11:32:53 +0800

    precourese dom

--- test function.js
+++ test function.js
@@ -1,6 +1,6 @@
 function countWords(message){
     var wordsArray = new Array();
-    wordsArray = message.split(/[^\w+]/);    // Wirte your code here
+    wordsArray = message.split(/[^\w+]/);
     var blankCount = 0;
     for (var i = 0; i < wordsArray.length; i++) {
         if (wordsArray[i] == "") {
@@ -9,5 +9,5 @@ function countWords(message){
     }
     return wordsArray.length - blankCount;
 }
-var num = countWords('Good morning, I love JavaScript.'); // should return 5
-console.log(num);
+countWords('Good morning, I love JavaScript.'); 
+
